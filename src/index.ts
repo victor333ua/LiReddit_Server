c8b2c646600ada831e0c066b7dbab9ecb6cd2ac4
VictorUA <vic333.ua@gmail.com>
2021-07-09 14:31:45 +0300

    express, apollo-server, type-graphql, post crud

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("hello world!!!!!")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from './constants';
+import { Post } from "./entities/Post";
+import microconfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostsResolver } from "./resolvers/posts";
+
+const main = async () => {
+    const orm = await MikroORM.init(microconfig);
+    await orm.getMigrator().up();
+
+    // const post = orm.em.create(Post, {title: 'my first post'});
+    // await orm.em.persistAndFlush(post);
+
+    // console.log('---------------------sql2---------------------');
+    // await orm.em.nativeInsert(Post, {title: 'my first post 2'});
+
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+
+    const app = express();
+
+    // app.get("/", (_, res) => {  
+    //     res.send("hello");
+    // });
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostsResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    })
+};
+
+main().catch((err) => {
+    console.error(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { __prod__ } from "./constants";
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+import { Post } from './entities/Post';
+
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    },
+    // entities: ['./dist/entities/*.js'], // path to your JS entities (dist), relative to `baseDir`
+    // entitiesTs: ['./src/entities/*.ts'],
+    entities: [Post],
+    dbName: "lireddit",
+    user: "postgres",
+    password: "victory3",
+    type: "postgresql",
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
